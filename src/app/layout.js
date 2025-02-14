7cdbfa431a1bdb2006c06ea00856dc9d9f49ee26
Matheus <matheushlsilva2000@gmail.com>
2025-02-14 17:03:07 -0300

    ALTERAÇÃO

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,10 @@ import { Bebas_Neue } from "next/font/google";
 import "./globals.css";
 import Image from 'next/image'
 import Nexos from '../public/nexos.svg' 
+import { Input } from "postcss";
+import  "bootstrap/dist/css/bootstrap.min.css"
+import AddBootstrap from "./AddBootstrap";
+
 const geits = Bebas_Neue({
   subsets: ['latin'],
   weight: "400",
@@ -16,7 +20,53 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body
-        className={`${geits.className}  antialiased`}>
+        className={`${geits.className} antialiased `}>
+          <AddBootstrap />
+
+          <nav className="navbar bg-body-tertiary fixed-top">
+  <div className="container-fluid">
+    <div className="flex flex-row-reverse ">
+    <h1 className="navbar-brand text-2xl w-64" >Nexos</h1>
+    <Image src={Nexos} width={50} alt="logo" />
+    </div>
+    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form className="d-flex mt-3" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
 
   
         {children}
