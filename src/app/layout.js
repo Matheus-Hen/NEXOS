7ef292d0e54d46759a836ffb9c75766837be0002
Matheus <matheushlsilva2000@gmail.com>
2025-02-14 16:55:56 -0300

    Start

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bebas_Neue } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Image from 'next/image'
+import Nexos from '../public/nexos.svg' 
+const geits = Bebas_Neue({
+  subsets: ['latin'],
+  weight: "400",
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -18,10 +14,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geits.className}  antialiased`}>
+
+  
         {children}
       </body>
     </html>
